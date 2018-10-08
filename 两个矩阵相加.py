ed8d5555efe992de9bc3620a6ed3099ce6539b2a
dream-blue <37263385+dream-blue@users.noreply.github.com>
2018-10-08 18:50:38 +0800

    Update 两个矩阵相加.py

--- 两个矩阵相加.py
+++ 两个矩阵相加.py
@@ -6,3 +6,19 @@ X = [[12, 7, 3],
 Y = [[5, 8, 1],
      [6, 7, 3],
      [4, 5, 9]]
+
+# 思路
+# fir_0 = X[0][0] + Y[0][0]
+# fir_1 = X[0][1] + Y[0][1]
+# print(fir_0, fir_1)
+li = []
+
+for i in range(3):
+    l = []
+    for j in range(3):
+        obj = X[i][j] + Y[i][j]
+        l.append(obj)
+    li.append(l)
+print(li)
+
+
